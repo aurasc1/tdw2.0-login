b51aba2602f8ef6cbe31659c04270d262f46b478
Aura <kris_rain_300@hotmail.com>
2015-10-07 10:29:24 -0500

    Cambios de la sesion6

--- index.php
+++ index.php
@@ -1,9 +1,24 @@
+<?php //Esto inicia codigo PHP
+$user = $_GET ["usuario"];//echo nos permite imprimir resultados (como el document.write)
+$pass = $_GET ["contraseña"];//echo nos permite imprimir resultados (como el document.write)
+//para dictar una variable en php es con el simbolo "$"
+if($user != "rctorr" || $pass != "1234567"){
+    echo "¡Lo siento, no estas autorizado!";
+} else {//inicia bloque autorizado
+    
+//Si el usuario y clave no coinciden entonces terminamos o enviamos un mensaje de error
+
+?>
+
+
+
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
      <link rel="stylesheet" href="css/main.css"> 
-   <title>Base</title>
+   <title>Felicidades</title>
 </head>
 
 <body>
@@ -20,11 +35,15 @@
 
         <div id="contenido">
             <p> ¡Felicidades, bienvenido al mundo!</p>
+         <div id="mensaje_noexiste" >
+          </div> <!-- termina mensaje-->  
         </div>  <!--termina contenido-->
         <footer>
              <p>Copyleft: TDW2.0</p>
-        
-        </footer>
+         </footer>
     
 </body>
 </html>
+<?php
+} //termina bloque autorizado
+?>
